c1dbcf2e7b1838bfce509bb088a6ef2350dde8ca
phamchinh2004 <phamchinh03112004@gmail.com>
2025-12-04 17:52:15 +0700

    Sửa sự kiện

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -840,9 +840,9 @@
                     <div class="notification-number">2</div>
                     <div class="special-event">
                         <div class="special-title">>>> Thông Báo Đặc Biệt <<< </div>
-                                <p><strong>Sự kiện mừng ngày 20/11 - Ngày Nhà Giáo Việt Nam</strong></p>
-                                <p>Khi khách hàng tham gia sự kiện đặc biệt, bạn có cơ hội nhận thưởng <span class="reward-amount">lên tới hàng triệu USD</span> cùng nhiều phần quà giá trị khác!</p>
-                                <p>Chương trình chỉ diễn ra trong dịp lễ 20/11 này, hãy nhanh tay tham gia để không bỏ lỡ cơ hội vàng.</p>
+                                <p><strong>Sự kiện mừng Giáng Sinh - Noel</strong></p>
+                                <p>Khi khách hàng tham gia sự kiện Noel đặc biệt, bạn có cơ hội nhận thưởng <span class="reward-amount">lên tới hàng triệu USD</span> cùng nhiều phần quà Giáng Sinh hấp dẫn!</p>
+                                <p>Chương trình chỉ diễn ra trong mùa lễ Giáng Sinh này, hãy nhanh tay tham gia để không bỏ lỡ cơ hội vàng.</p>
                         </div>
                     </div>
                 </div>
